c61555286b9faf8c8d7d69f33623bc04ffc2acce
danirebollo <dani@danirebollo.es>
2022-07-11 20:42:04 +0000

    autoupdate css

--- webresources/web_htmlparts_2.php
+++ webresources/web_htmlparts_2.php
@@ -3,6 +3,10 @@ include "webresources/web_custom_.php";
 
 function printhtmlheader()
 {
+    
+    $fecha = new DateTime();
+    $timestamp=$fecha->getTimestamp();
+
     $config = new reConfig();
 
     echo  <<<HTML
@@ -21,7 +25,7 @@ function printhtmlheader()
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="assets/grid.css?20">
+<link rel="stylesheet" href="assets/grid.css?{$timestamp}">
 </head>
 
 <body class="grid-container">
